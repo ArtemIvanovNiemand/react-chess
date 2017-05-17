401dfe7e7f709e7eb44d86d8acab5e26420d0101
ArtemIvanovNiemand <grishasial228@gmail.com>
2017-05-17 16:55:51 +0400

    Add css

--- sandbox/webpack.config.js
+++ sandbox/webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
-var NpmInstallPlugin = require('npm-install-webpack-plugin');
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -17,7 +19,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new NpmInstallPlugin() 
+    new NpmInstallPlugin()
   ],
   module: {
     preLoaders: [
@@ -29,7 +31,7 @@ module.exports = {
         ],
       }
     ],
-    loaders: [ //все остальное осталось не тронутым
+    loaders: [
       {
         loaders: ['react-hot', 'babel-loader'],
         include: [
@@ -37,7 +39,14 @@ module.exports = {
         ],
         test: /\.js$/,
         plugins: ['transform-runtime'],
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
     ]
+  },
+  postcss: function () {
+    return [autoprefixer, precss];
   }
 }
