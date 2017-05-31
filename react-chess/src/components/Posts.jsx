dfbdb4721baf762fdd9a90367fdb7d36657d4c6a
ArtemIvanovNiemand <grishasial228@gmail.com>
2017-05-31 11:59:35 +0400

    Refactor posts

--- react-chess/src/components/Posts.jsx
+++ react-chess/src/components/Posts.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-const Posts = ({posts}) => (
-  <ul>
-    {posts.map((post, i) =>
-      <li key={i}>{post.title}</li>
-    )}
-  </ul>
-)
+export default class Posts extends Component {
+  static propTypes = {
+    posts: PropTypes.array.isRequired
+  };
+  generatePosts(posts){
+    return(
+      <ul>
+        {posts.map((post, i) => <li key={i}>{post.title}</li>)}
+      </ul>
+    );
+  }
 
-Posts.propTypes = {
-  posts: PropTypes.array.isRequired
+  render() {
+    const { posts } = this.props;
+    const lines = this.generatePosts(posts);
+
+    return lines;
+  }
 }
-
-export default Posts
